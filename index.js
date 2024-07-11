0d1dc92da0dc2a793577069bc65297e9f2686d64
Aliyv019 <tahiraliyev2006@gmail.com>
2024-07-11 20:43:25 +0400

    added new edit method

--- index.js
+++ index.js
@@ -7,23 +7,38 @@ console.log(myarr);
 
 add_btn.addEventListener('click',()=>{
     const ListItem=document.createElement('li')
-    ListItem.innerHTML=`<p>${input.value}</p>
+    ListItem.innerHTML=`<div class="listitem" style="display: flex;width: 100%;justify-content: space-between;">
+                    <p>${input.value}</p>
                 <div>
                 <button>Edit</button>
                 <button>Delete</button>
+                </div>
+                </div>
+                <div class="editlistitem" style="display: none;justify-content: space-between;">
+                    <input type="text">
+                    <button>Done</button>
                 </div>`
     list.appendChild(ListItem)
     const edit_btn=ListItem.querySelectorAll('button')[0]
     const delete_btn=ListItem.querySelectorAll('button')[1]
+    const editlistitem=ListItem.querySelector('.editlistitem')
+    const listiteminside=ListItem.querySelector('.listitem')
+    const done_btn=ListItem.querySelectorAll('button')[2]
+    const edit_input=ListItem.querySelector('input')
     edit_btn.addEventListener('click',()=>{
-        const new_todo=prompt("enter new todo:")
-        myarr.forEach((item, index) => {
-            if (item == ListItem.querySelector('p').textContent) {
-                myarr[index]=new_todo
-                ListItem.querySelector('p').textContent=new_todo
-                localStorage.setItem('todos', JSON.stringify(myarr));
-            }
-        });
+        editlistitem.style.display='flex'
+        listiteminside.style.display='none'
+        myarr.forEach((element,index)=>{
+            done_btn.addEventListener('click',()=>{
+                if(element == ListItem.querySelector('p').textContent){
+                    myarr[index]=edit_input.value
+                    ListItem.querySelector('p').textContent=edit_input.value
+                    editlistitem.style.display='none'
+                    listiteminside.style.display='flex'
+                    localStorage.setItem('todos', JSON.stringify(myarr));
+                }
+            })
+        })
     })
     myarr.push(input.value)
     delete_btn.addEventListener('click',()=>{
@@ -39,28 +54,42 @@ add_btn.addEventListener('click',()=>{
     input.value=""
 })
 
-list.innerHTML = "";
+// list.innerHTML = "";
 myarr.forEach((element) => {
     console.log(element);
     const ListItem = document.createElement('li');
-    ListItem.innerHTML = `<p>${element}</p>
+    ListItem.innerHTML = `<div class="listitem" style="display: flex;width: 100%;justify-content: space-between;">
+                    <p>${element}</p>
                 <div>
                 <button>Edit</button>
                 <button>Delete</button>
+                </div>
+                </div>
+                <div class="editlistitem" style="display: none;justify-content: space-between;">
+                    <input type="text">
+                    <button>Done</button>
                 </div>`;
     list.appendChild(ListItem);
     const edit_btn=ListItem.querySelectorAll('button')[0]
     const delete_btn = ListItem.querySelectorAll('button')[1]
+    const editlistitem=ListItem.querySelector('.editlistitem')
+    const listiteminside=ListItem.querySelector('.listitem')
+    const done_btn=ListItem.querySelectorAll('button')[2]
+    const edit_input=ListItem.querySelector('input')
     edit_btn.addEventListener('click',()=>{
-        const new_todo=prompt("enter new todo:")
-        myarr.forEach((item, index) => {
-            if (item == ListItem.querySelector('p').textContent) {
-                myarr[index] = new_todo;
-                ListItem.querySelector('p').textContent = new_todo;
-                localStorage.setItem('todos', JSON.stringify(myarr));
-            }
-        });
-        localStorage.setItem('todos', JSON.stringify(myarr));
+        editlistitem.style.display='flex'
+        listiteminside.style.display='none'
+        myarr.forEach((element,index)=>{
+            done_btn.addEventListener('click',()=>{
+                if(element == ListItem.querySelector('p').textContent){
+                    myarr[index]=edit_input.value
+                    ListItem.querySelector('p').textContent=edit_input.value
+                    editlistitem.style.display='none'
+                    listiteminside.style.display='flex'
+                    localStorage.setItem('todos', JSON.stringify(myarr));
+                }
+            })
+        })
     })
     delete_btn.addEventListener('click', () => {
         ListItem.remove();
